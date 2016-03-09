f5a6f1ad76412eb562c5fc9ecee86914a486395d
Manu <fmperez89@gmail.com>
2016-03-09 09:49:31 +0100

    Creacion portlet A sin programacion de eventos

---
+++ src/main/java/com/examen/portlet/todos/PortletA.java
@@ -0,0 +1,70 @@
+package com.examen.portlet.todos;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.ProcessAction;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+/**
+ * Portlet implementation class PortletA
+ */
+public class PortletA extends GenericPortlet {
+	
+	public static String NOMBRE = "nombre";
+	public static String DIRECCION = "direccion";
+	public static String TELEFONO = "telefono";
+		
+	protected String viewTemplate;
+
+    private static Log _log = LogFactoryUtil.getLog(PortletA.class);
+	
+
+    public void init() {
+        viewTemplate = getInitParameter("view-template");
+    }
+
+    public void doView(
+            RenderRequest renderRequest, RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        include(viewTemplate, renderRequest, renderResponse);
+    }
+
+    protected void include(
+            String path, RenderRequest renderRequest,
+            RenderResponse renderResponse)
+        throws IOException, PortletException {
+
+        PortletRequestDispatcher portletRequestDispatcher =
+            getPortletContext().getRequestDispatcher(path);
+
+        if (portletRequestDispatcher == null) {
+            _log.error(path + " is not a valid include");
+        }
+        else {
+            portletRequestDispatcher.include(renderRequest, renderResponse);
+        }
+    }
+ 
+    @ProcessAction(name="irAPortletB")
+    
+    @Override
+    public void processAction(ActionRequest request, ActionResponse response) throws PortletException, IOException {
+    	// TODO Auto-generated method stub
+    	super.processAction(request, response);
+    }
+    
+    
+    
+    
+
+}
